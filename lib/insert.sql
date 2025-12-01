1772d81bdb389396a2855e554b88f058c3c2b6ed
Derrickkoome <kmderrickkimathi@gmail.com>
2025-12-01 12:30:21 +0300

    Complete Organizing Bears Lab: implement CREATE, INSERT, and SELECT queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 15, 'M', 'brown', 'aggressive', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 18, 'F', 'black', 'friendly', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 25, 'M', 'brown', 'disciplined', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 12, 'F', 'white', 'playful', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 30, 'M', 'green', 'grumpy', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 22, 'F', 'blonde', 'curious', 1);
+INSERT INTO bears VALUES (8, NULL, 10, 'F', 'gray', 'shy', 1);
